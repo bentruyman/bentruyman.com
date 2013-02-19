2ee2bde2947aa1810d49a9c37197556d0dc639a9
Ben Truyman <bentruyman@gmail.com>
2013-02-18 20:54:16 -0600

    Did something really silly for performance

--- plugins/ext.rb
+++ plugins/ext.rb
@@ -2,3 +2,18 @@ require 'zurb-foundation'
 require 'animation'
 require "jekyll-assets"
 require "jekyll-assets/compass"
+
+# override jekyll-assets plugin to add defer/async attributes
+module Jekyll
+  module AssetsPlugin
+    class Renderer
+      JAVASCRIPT = '<script type="text/javascript" src="%s" defer async></script>'
+      
+      def render_javascript
+        @path << ".js" if File.extname(@path).empty?
+        
+        JAVASCRIPT % render_asset_path
+      end
+    end
+  end
+end
